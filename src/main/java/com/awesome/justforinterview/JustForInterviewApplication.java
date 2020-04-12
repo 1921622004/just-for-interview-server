92ab97badfb3bae70ebe802bd4973f2286eba444
1921622004 <1921622004qwer@gmail.com>
2020-04-12 22:04:43 +0800

    init project

---
+++ src/main/java/com/awesome/justforinterview/JustForInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.awesome.justforinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustForInterviewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JustForInterviewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/awesome/justforinterview/JustForInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome.justforinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustForInterviewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
