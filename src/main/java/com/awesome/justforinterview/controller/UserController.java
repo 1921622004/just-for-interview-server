34d6b968ba56fd7f427ca5b4bb79ae32e83985fb
1921622004 <1921622004qwer@gmail.com>
2020-04-13 22:14:32 +0800

    user 跑通

--- src/main/java/com/awesome/justforinterview/controller/UserController.java
+++ src/main/java/com/awesome/justforinterview/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.awesome.justforinterview.controller;
 
+import com.awesome.justforinterview.model.User;
 import com.awesome.justforinterview.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
   @Autowired
   private UserService userService;
+
+  @RequestMapping(value = "/detail/{id}")
+  public User getUserById(@PathVariable String id) {
+    return this.userService.getUserById(id);
+  }
+
+  @RequestMapping(value = "/test")
+  public String test() {
+    return "test";
+  }
 }
